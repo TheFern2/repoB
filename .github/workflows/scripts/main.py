507639c67495d518c8595fcdd040e0b2bb16571a
Fernando Balandran <fernandobe+git@protonmail.com>
2024-06-18 11:45:03 -0500

    add tag to payload

--- .github/workflows/scripts/main.py
+++ .github/workflows/scripts/main.py
@@ -3,6 +3,7 @@ import json
 import os
 
 TOKEN = os.environ['MY_TOKEN']
+GITHUB_REF = os.environ.get('GITHUB_REF', '')
 
 def check_token(token):
     print(f"Token starts with: {token[:5]}")
@@ -16,7 +17,7 @@ def check_token(token):
     else:
         print("Token is invalid.")
 
-def trigger_workflow(repo_owner, repo_name, token):
+def trigger_workflow(repo_owner, repo_name, token, tag):
     url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/dispatches"
     headers = {
         "Accept": "application/vnd.github.everest-preview+json",
@@ -25,8 +26,7 @@ def trigger_workflow(repo_owner, repo_name, token):
     payload = {
         "event_type": "trigger-event",
         "client_payload": {
-            "key1": "value1",
-            "key2": "value2"
+            "version_tag": tag
         }
     }
     response = requests.post(url, headers=headers, data=json.dumps(payload))
@@ -41,6 +41,12 @@ repo_owner = "TheFern2"
 repo_name = "repoA"
 token = TOKEN
 
+# Extract the tag from GITHUB_REF
+if GITHUB_REF.startswith('refs/tags/'):
+    version_tag = GITHUB_REF[len('refs/tags/'):]
+else:
+    version_tag = 'unknown'
+
 check_token(token)
 
-trigger_workflow(repo_owner, repo_name, token)
+trigger_workflow(repo_owner, repo_name, token, version_tag)
